90b10c3bd297632b50b8764a3bd8daa7c94053f3
matthew-eiley <matthew.eiley@mail.mcgill.ca>
2025-07-02 13:37:23 -0400

    added day of week and week of year logic to sleep

--- sleep.py
+++ sleep.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 
 FILENAME = "data.csv"
@@ -12,4 +13,24 @@ df["hours_vs_needed_rgb"] = df.apply(lambda row: get_rGb(row, "hours_vs_needed")
 df["sleep_consistency_rgb"] = df.apply(lambda row: get_rGb(row, "sleep_consistency"), axis=1)
 
 print(df)
+
+# grid[week][day] to access a specific cell (52 weeks, 7 days)
+grid = []
+for week in range(52):
+    grid.append([])
+    for day in range(7):
+        grid[week].append(None)
+
+def get_week_and_day(date):
+    # find day
+    day = date.isocalendar().weekday - 1 # monday=0 , sunday=7
+    # find week
+    today = datetime.today()
+    today_week_number = today.isocalendar().week
+    date_week_number = date.isocalendar().week
+    if today.year == date.year:
+        week_number = 52 - (today_week_number - date_week_number)
+    else:
+        week_number = 52 - ((today_week_number + 52) - date_week_number)
+    print(week_number)
 
