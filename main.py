98609d5b723bc2e406aa45194ac446b60caa4122
matthew-eiley <matthew.eiley@mail.mcgill.ca>
2025-07-02 10:22:15 -0400

    entered all past data

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ FILENAME = "data.csv"
 def get_date():
     date = input("Input the date for which you are entering values.\nIf you just want to use today's date, press enter.\nIf you want to use a different date, enter it in the form mm/dd/yyyy:\n")
     if date == "":
-        date = datetime.today()
+        today = datetime.today()
+        date = datetime(month=today.month, day=today.day, year=today.year)
     else:
         specs = date.split("/")
         date = datetime(month=int(specs[0]), day=int(specs[1]), year=int(specs[2]))
     return date
 
-def get_int_input(prompt):
-    return int(input(prompt + ":\n"))
+def get_float_input(prompt):
+    return float(input("\n" + prompt + ":\n"))
 
-def main():
+def get_and_store_data():
     entry = {
         "date": get_date(),
-        "hours_vs_needed": get_int_input("Sleep vs needed (%)"),
-        "sleep_consistency": get_int_input("Sleep consistency (%)"),
-        "recovery": get_int_input("Recovery (%)"),
-        "strain": get_int_input("Strain"),
-        "hrv": get_int_input("Heart Rate Variability (ms)")
+        "hours_vs_needed": get_float_input("Sleep vs needed (%)"),
+        "sleep_consistency": get_float_input("Sleep consistency (%)"),
+        "recovery": get_float_input("Recovery (%)"),
+        "strain": get_float_input("Strain"),
+        "hrv": get_float_input("Heart Rate Variability (ms)")
     }
 
     if os.path.exists(FILENAME):
@@ -34,6 +35,9 @@ def main():
     df = pd.concat([df, pd.DataFrame([entry])], ignore_index=True)
     df.sort_values("date", inplace=True)
     df.to_csv(FILENAME, index=False)
-    print("Entry saved.")
+    print("\nEntry saved.")
+
+def main():
+    get_and_store_data()
 
 main()
